58c38faf8b73bb57f7afcc4a41d90372cdf721c1
laura ajala <lauraq2000@hotmail.com>
2024-05-02 15:10:42 -0400

    Add files via upload

---
+++ switch1.php
@@ -0,0 +1,37 @@
+<?php
+$diadasemana = date("w");
+
+	switch ($diadasemana) {
+
+		case 0:
+		echo "domingo";
+		break;
+
+		case 1:
+		echo "segunda-feira";
+		break;
+
+		case 2: 
+		echo "terça-feira";
+		break;
+
+		case 3:
+		echo "quarta-feira";
+		break;
+
+		case 4: 
+		echo "quinta-feira";
+		break;
+
+		case 5:
+		echo "sexta-feira";
+		break;
+
+		case 6:
+		echo "sabado";
+		break;
+
+	}
+
+
+	 ?>
